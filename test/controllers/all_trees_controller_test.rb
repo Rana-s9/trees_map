884b2b1cc964ae22305e0bcd4bcc4c1ab1ffbc6a
rana <nomumumu561@gmail.com>
2025-04-28 16:30:42 +0900

    add/植林ログ

---
+++ test/controllers/all_trees_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class AllTreesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get all_trees_index_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get all_trees_edit_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get all_trees_show_url
+    assert_response :success
+  end
+end
---
+++ test/controllers/my_trees_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class MyTreesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get my_trees_index_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get my_trees_edit_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get my_trees_show_url
+    assert_response :success
+  end
+end
