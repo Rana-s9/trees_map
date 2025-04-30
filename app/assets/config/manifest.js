074da9563c600b61f401b546a633f3935d88447f
rana <nomumumu561@gmail.com>
2025-04-30 22:19:14 +0900

    add/manifest.js

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+//= link application.css
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+//= link top.js
+//= link trees.js
+//= link camera.js
