67787ab945e94a2963fbf7ae2d0386e955c1e1d5
rana <nomumumu561@gmail.com>
2025-05-10 12:54:10 +0900

    add/validate_position

--- app/models/fav_place.rb
+++ app/models/fav_place.rb
@@ -4,8 +4,14 @@ class FavPlace < ApplicationRecord
 
   validates :address, presence: true, uniqueness: true
   validates :fav_name, presence: true, uniqueness: true
+  validate :position_place
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
+  def position_place
+    if fav_x.blank? || fav_y.blank? || fav_z.blank?
+      errors.add(:base, '座標取得のため、もう一度お試しください')
+    end
+  end
 end
--- app/models/tree.rb
+++ app/models/tree.rb
@@ -3,4 +3,11 @@ class Tree < ApplicationRecord
   belongs_to :fav_place, optional: true # fav_placeなしでも植えられる場合は optional: true を追加
 
   validates :tree_name, presence: true
+  validate :position_tree
+
+  def position_tree
+    if position_x.blank? || position_y.blank? || position_z.blank?
+      errors.add(:base, '座標情報が不足しています。恐れ入りますが、場所登録からもう一度お試しください')
+    end
+  end
 end
